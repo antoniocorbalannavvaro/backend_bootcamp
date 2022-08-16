be25430aa03538120658796c59507b2c7d079051
Antonio Corbalán Navarro <antoniocn1996@gmail.com>
2022-08-16 22:08:07 +0200

    Add files via upload

---
+++ entregas/ejercicio.js
@@ -0,0 +1,26 @@
+/*
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+
+const name = 'Antonio';
+let age = 26;
+let isDeveloper = true;
+const birthday = new Date('January 25, 1996 06:30:01')
+
+const favBook =
+{
+    title: 'El nombre del viento',
+    author: 'Patrick Rothfuss',
+    date: '27/03/2007',
+    url: 'https://es.wikipedia.org/wiki/El_nombre_del_viento'  
+}
